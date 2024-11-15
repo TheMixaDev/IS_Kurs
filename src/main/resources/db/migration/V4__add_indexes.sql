27a561db5b2c91bdcc22a8a98cdd0537f4282ff3
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-11-15 21:07:59 +0300

    remove migration

--- src/main/resources/db/migration/V4__add_indexes.sql
+++
@@ -1,6 +0,0 @@
---- Ускорение поиска спринтов по году
-CREATE INDEX sprint_start_date_year_idx ON Sprint (EXTRACT(YEAR FROM start_date));
---- Ускорение поиска спринтов по команде
-CREATE INDEX sprint_team_id_idx ON Sprint (team_id);
---- Ускорение поиска задач по спринту
-CREATE INDEX task_sprint_id_idx ON Task (sprint_id);
