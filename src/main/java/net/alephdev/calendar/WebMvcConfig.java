464232371259a3366ebd94447ed73e71a8ba16c7
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-11-15 21:09:05 +0300

    change frontend port

--- src/main/java/net/alephdev/calendar/WebMvcConfig.java
+++ src/main/java/net/alephdev/calendar/WebMvcConfig.java
@@ -37,7 +37,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
