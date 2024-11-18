04d99296374b000a857b2f5dc1f1525585ca84a3
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-11-18 21:08:52 +0300

    role status prefabs
    known issue: tooltip

--- angular/src/app/views/role/role.component.ts
+++ angular/src/app/views/role/role.component.ts
@@ -1,9 +1,25 @@
 import {Component, OnInit} from "@angular/core";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {PrimaryButtonBinding} from "../../components/bindings/primary-button.binding";
+import {TableCellComponent} from "../../components/table/table-cell.component";
+import {TableComponent} from "../../components/table/table.component";
+import {TableRowComponent} from "../../components/table/table-row.component";
+import {TooltipBinding} from "../../components/bindings/tooltip.binding";
+import {UiButtonComponent} from "../../components/ui/ui-button.component";
+import {faEdit, faPlus, faStar, faTrash} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent,
+    PrimaryButtonBinding,
+    TableCellComponent,
+    TableComponent,
+    TableRowComponent,
+    TooltipBinding,
+    UiButtonComponent
+  ],
   templateUrl: './role.component.html'
 })
 export class RoleComponent implements OnInit {
@@ -12,4 +28,9 @@ export class RoleComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  protected readonly faPlus = faPlus;
+  protected readonly faStar = faStar;
+  protected readonly faEdit = faEdit;
+  protected readonly faTrash = faTrash;
 }
--- angular/src/app/views/status/status.component.ts
+++ angular/src/app/views/status/status.component.ts
@@ -1,9 +1,31 @@
 import {Component, OnInit} from "@angular/core";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {HeaderItemBinding} from "../../components/bindings/header-item.binding";
+import {PrimaryButtonBinding} from "../../components/bindings/primary-button.binding";
+import {UiDropdownComponent} from "../../components/ui/ui-dropdown.component";
+import {faCircle, faEdit, faGear, faListCheck, faPlus, faStar, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {DatePipe} from "@angular/common";
+import {TableCellComponent} from "../../components/table/table-cell.component";
+import {TableComponent} from "../../components/table/table.component";
+import {TableRowComponent} from "../../components/table/table-row.component";
+import {TooltipBinding} from "../../components/bindings/tooltip.binding";
+import {UiButtonComponent} from "../../components/ui/ui-button.component";
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent,
+    HeaderItemBinding,
+    PrimaryButtonBinding,
+    UiDropdownComponent,
+    DatePipe,
+    TableCellComponent,
+    TableComponent,
+    TableRowComponent,
+    TooltipBinding,
+    UiButtonComponent
+  ],
   templateUrl: './status.component.html'
 })
 export class StatusComponent implements OnInit {
@@ -12,4 +34,12 @@ export class StatusComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  protected readonly faPlus = faPlus;
+  protected readonly faListCheck = faListCheck;
+  protected readonly faEdit = faEdit;
+  protected readonly faTrash = faTrash;
+  protected readonly faCircle = faCircle;
+  protected readonly faGear = faGear;
+  protected readonly faStar = faStar;
 }
