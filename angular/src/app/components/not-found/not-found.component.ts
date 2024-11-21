ebee28e27519c967a0003f6a27625182ea7a4766
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-11-22 02:58:00 +0300

    fix 404

--- angular/src/app/components/not-found/not-found.component.ts
+++ angular/src/app/components/not-found/not-found.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import {LoaderService} from "../../services/loader.service";
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
+  standalone: true
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  constructor(private loaderService: LoaderService) {
+    this.loaderService.loader(false);
+  }
+}
